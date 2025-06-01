25357d949c8ad761acf0ebb07d04c829c9f4029b
Dani <di.avendanopinto@gmail.com>
2025-06-01 14:04:01 -0400

    tarea3: lista de los episodios

--- opcional.py
+++ opcional.py
@@ -17,19 +17,19 @@ for n in characters["results"]:
 #print(status_personaje)
 
 # 3.- Un método capaz de obtener TODOS los episodios. Gu´ardelos en una lista llamada episodios dentro de la clase.
-#for e in characters["results"]:
-    #episodio = requests.get(e["episode"]).json()
-    #print(episodio) 
-
-
-
+episodios = []
+for e in characters["results"]:
+    capitulos = e["episode"]
+    for i in capitulos:
+        info = requests.get(i).json() #['id', 'name', 'air_date', 'episode', 'characters', 'url', 'created']
+        episodio = info["episode"]
+        episodios.append(episodio)
+#print(episodios)
 #4.- crear una lista de diccionario que muestre: "[{temporada 1: 12 episodios}]" algo así.
 
 
-
 #5.- Un método capaz de obtener todos los planetas y estaciones espaciales que se muestran en el endpoint location.
-for n in characters["results"]:
-    print(n["location"])
+
 
 #6.- El hilo principal, en donde usted llamar´a a la clase RAMExtractor, debe iterar sobre las páginas de resultados.
 
