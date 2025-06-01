98c2365e0061fd60f9527d49758b5c0ef5201937
Dani <di.avendanopinto@gmail.com>
2025-06-01 10:37:53 -0400

    mostrar personaje y estado sin list-comprehesion

--- opcional.py
+++ opcional.py
@@ -1,7 +1,6 @@
 import json
 import requests
 
-
 url = 'https://rickandmortyapi.com/api'
  # {'characters': 'url', 'locations': 'url', 'episodes': 'url'}
 response = requests.get(url) 
@@ -10,6 +9,10 @@ api_json = json.loads(response.text)
 characters = requests.get(api_json["characters"]).json() # ['info', 'results']
 #characters["results"] = ['id', 'name', 'status', 'species', 'type', 'gender', 'origin', 'location', 'image', 'episode', 'url', 'created']
 
+status_personaje = []
+for n in characters["results"]:
+    name = n["name"]
+    status = n["status"]
+    status_personaje.append((name,status))
 
-status_personaje = [(n["name"], n["status"]) for n in characters["results"]]
 print(status_personaje)
