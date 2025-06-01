1d8c2b966d5d6b5abc3ba09bc7c0cf06add0c8d1
Dani <di.avendanopinto@gmail.com>
2025-06-01 10:57:39 -0400

    tareas ha realizar

--- opcional.py
+++ opcional.py
@@ -14,5 +14,30 @@ for n in characters["results"]:
     name = n["name"]
     status = n["status"]
     status_personaje.append((name,status))
+#print(status_personaje)
 
-print(status_personaje)
+# 3.- Un método capaz de obtener TODOS los episodios. Gu´ardelos en una lista llamada episodios dentro de la clase.
+#for e in characters["results"]:
+    #episodio = requests.get(e["episode"]).json()
+    #print(episodio) 
+
+
+
+#4.- crear una lista de diccionario que muestre: "[{temporada 1: 12 episodios}]" algo así.
+
+
+
+#5.- Un método capaz de obtener todos los planetas y estaciones espaciales que se muestran en el endpoint location.
+for n in characters["results"]:
+    print(n["location"])
+
+#6.- El hilo principal, en donde usted llamar´a a la clase RAMExtractor, debe iterar sobre las páginas de resultados.
+
+
+#7.- Imprima los índices que ocupan los personajes que en su nombre lleven ”Rick” y ”Morty”.
+
+#8.- Si en el ítem anterior usted es capaz de hacer que la impresión en pantalla
+#muestre el índice y además el nombre de los personajes de manera “bonita”, obtiene 10
+#puntos más. Por otro lado, si es capaz de dejar en listas separadas a estos personajes
+#e imprimir de manera “bonita” cada lista, obtiene 10 puntos adicionales.
+
