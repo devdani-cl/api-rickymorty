4e167e169e4f372c8f64abe6f46aa891bdd41da9
Dani <di.avendanopinto@gmail.com>
2025-05-31 23:03:24 -0400

    solucion: mostrar personaje y estado

--- opcional.py
+++ opcional.py
@@ -10,20 +10,6 @@ api_json = json.loads(response.text)
 characters = requests.get(api_json["characters"]).json() # ['info', 'results']
 #characters["results"] = ['id', 'name', 'status', 'species', 'type', 'gender', 'origin', 'location', 'image', 'episode', 'url', 'created']
 
-'''
-mi duda al usar list-comprehesion es, 
-¿puedo usar la lista creada para acceder a otras llaves? ejemplo: 'status', 'species', 'origin', etc
-'''
 
-personajes = [n["name"] for n in characters["results"]]
-status = [s["status"] for s in characters["results"]]
-
-status_personaje = []
-for n in personajes:
-    name = n 
-    for s in status:
-        estado = s
-        status_personaje.append((name,estado))
-
+status_personaje = [(n["name"], n["status"]) for n in characters["results"]]
 print(status_personaje)
-
